71ded9f79136c17d60dddc302a9c5ff1dec599e7
Andrej Amelichkin <ainshtein2004@gmail.com>
2017-02-23 02:25:57 +0200

    start template

---
+++ src/js/partials/slider.js
@@ -0,0 +1,12 @@
+// -------- SLIDER ------- //
+;$(document).ready(function() {
+
+  $("#slider").owlCarousel({
+
+      navigation : true,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem : true,
+      autoPlay : true
+  });
+});
